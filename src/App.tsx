d0bfb6445a8b9f474a15d7651ed8d39f0f83a4f7
RabeDatta <robedutt@gmail.com>
2023-02-05 16:24:51 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import "./App.css";
-
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <section>
+        <h3> TESTING </h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+          molestiae iusto voluptas, quam perspiciatis harum dolores inventore
+          omnis explicabo aliquam sed quos unde possimus recusandae nemo,
+          ratione laborum ipsam quas.
+        </p>
+      </section>
+    </div>
+  );
 }
 
 export default App;
